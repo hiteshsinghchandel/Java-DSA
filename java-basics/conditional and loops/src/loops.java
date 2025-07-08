30587601096423f7ca821a4843a7c57916d034b0
hiteshsinghchaandel <hiteshsingh0219@gmail.com>
2025-07-08 22:35:47 +0530

    this is about loops statements

--- java-basics/conditional and loops/src/loops.java
+++ java-basics/conditional and loops/src/loops.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // lopps are used when we have to do a work in continuous manner!!
+        // there are 3 types of loops
+        // 1. while loop
+        // 2. do while loop
+        // for loop
         System.out.print("enter the value of n : ");
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
