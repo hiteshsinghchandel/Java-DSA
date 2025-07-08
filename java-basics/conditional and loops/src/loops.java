d9697b285f51749d6f6ad70480b854c43ef56d4d
hiteshsinghchaandel <hiteshsingh0219@gmail.com>
2025-07-08 22:26:36 +0530

    this is about loops statements

---
+++ java-basics/conditional and loops/src/loops.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the value of n : ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("Hello!!");
+
+        }
+    }
+}
